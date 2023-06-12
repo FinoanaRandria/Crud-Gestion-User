64eb07e5919c5ba6bddb4f35458f6f1e24f62db1
FinoanaRandria <fitiavanarex4@gmail.com>
2023-06-12 18:53:01 +0300

    add client + config

---
+++ client/src/pages/AddEdit.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const AddEdit = () => {
+  return (
+    <>
+      <h2>AddEdit</h2>
+    </>
+  )
+}
+
+export default AddEdit
---
+++ client/src/pages/View.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const View = () => {
+  return (
+    <>
+    
+       <h2>View</h2>
+    </>
+  )
+}
+
+export default View
